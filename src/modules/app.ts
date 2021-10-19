d087345649d83a36686e7d313c4f55c244519582
code0450 <atomsole567@gmail.com>
2021-10-19 15:01:12 +0500

    initialized the bot and disable implicit any in tsconfig

--- src/modules/app.ts
+++ src/modules/app.ts
@@ -1,1 +1,9 @@
-import * as TelegramBot from 'node-telegram-bot-api'
+import TelegramBot from 'node-telegram-bot-api'
+
+class App {
+    private bot;
+
+    initialize() {
+        this.bot = new TelegramBot(process.env.TOKEN as string, { polling: true });
+    }
+}
