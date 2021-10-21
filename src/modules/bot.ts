5606a52ac038b871e633ceac6c2051ddfb21ba05
code0450 <atomsole567@gmail.com>
2021-10-21 15:19:21 +0500

    xz

--- src/modules/bot.ts
+++ src/modules/bot.ts
@@ -4,14 +4,7 @@ export class Bot {
     private bot;
     private options;
 
-    constructor() {
-        this.options = {
-            webHook: {
-                port: process.env.PORT
-            }
-        };
-    }
-
+    
     activate() {
         this.bot = new TelegramBot(process.env.TOKEN as string, this.options);
         this.bot.setWebHook(`https://botn12.herokuapp.com:443/bot${process.env.TOKEN}`);
