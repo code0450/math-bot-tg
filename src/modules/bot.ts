e61312ab8dd51729a76d08ee321ef4393ed67e59
code0450 <atomsole567@gmail.com>
2021-10-21 21:11:02 +0500

    cx

--- src/modules/bot.ts
+++ src/modules/bot.ts
@@ -2,10 +2,9 @@ import TelegramBot from 'node-telegram-bot-api'
 
 export class Bot {
     private bot;
-    private options;
 
     activate() {
-        this.bot = new TelegramBot(process.env.TOKEN as string, this.options);
+        this.bot = new TelegramBot(process.env.TOKEN as string);
         this.bot.setWebHook(`https://botn12.herokuapp.com:443/bot${process.env.TOKEN}`);
     }
 }
