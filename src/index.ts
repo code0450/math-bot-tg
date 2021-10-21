20db21a7ef284420a9f51e3c81a0954f7b2a4f45
code0450 <atomsole567@gmail.com>
2021-10-21 15:02:24 +0500

    dotenv removed

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import * as dotenv from 'dotenv'
 import { Bot } from './modules/bot'
 import { Server } from './modules/server'
 
-dotenv.config();
+
 const bot = new Bot();
 bot.activate();
 const server = new Server();
