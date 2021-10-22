c27db39724d03ab9701acdd34dfdf44406798f55
code0450 <atomsole567@gmail.com>
2021-10-22 13:58:06 +0500

    trigger set

--- src/modules/bot.ts
+++ src/modules/bot.ts
@@ -1,4 +1,5 @@
-import TelegramBot from 'node-telegram-bot-api'
+import TelegramBot from 'node-telegram-bot-api';
+import { Start } from '../commands/start';
 
 export class Bot {
     private bot;
@@ -15,6 +16,8 @@ export class Bot {
     activate() {
         this.bot = new TelegramBot(process.env.TOKEN as string);
         this.bot.setWebHook(`https://talking-bot-0.herokuapp.com:443/bot${process.env.TOKEN}`);
+        
+        new Start().init();
     }
 
     getBot() {
