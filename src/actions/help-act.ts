210606bb3cf4744e866d6d1265bbf0e11e9467f4
code0450 <atomsole567@gmail.com>
2021-10-23 17:04:19 +0500

    ss

--- src/actions/help-act.ts
+++ src/actions/help-act.ts
@@ -11,7 +11,7 @@ export class HelpAct {
         this.bot.onText(/\/help/, msg => {
             this.bot.sendMessage(msg.chat.id, 'Each level consists of 10 audio questions, ' + 
                 'your objective is to solve all of them in 90 seconds. ' +
-                'Start from the level 1 and move on as you beat the time')
+                'Start from the level 1 and move on as you beat the time.')
         })
     }
 }
