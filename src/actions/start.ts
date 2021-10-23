e7240d811e19bb9c90a3c575758f0425e4a821a2
code0450 <atomsole567@gmail.com>
2021-10-23 16:46:37 +0500

    sss

--- src/actions/start.ts
+++ src/actions/start.ts
@@ -10,8 +10,8 @@ export class Start {
     init() {
         this.bot.onText(/\/start/, msg => {
             this.bot.sendMessage(msg.chat.id, 'Hello, I will help you to improve your arithmetic skills.');
-            this.bot.sendMessage(msg.chat.id, '  /game - to launch the game  \n' + 
-                '  /help - to get info about the game  ')
+            this.bot.sendMessage(msg.chat.id, '/game - to launch the game\n' + 
+                '/help - to get info about the game')
         })
     }
 }
