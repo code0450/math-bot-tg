c0f73ba9139da706487950d385cef23e652bf0cc
code0450 <atomsole567@gmail.com>
2021-10-23 16:15:18 +0500

    cc

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -9,8 +9,8 @@ export class Start {
 
     init() {
         this.bot.onText(/\/start/, msg => {
-            this.bot.sendMessage(msg.chat.id, 'Hello, I will help you to improve your arithmetic skills.\n' + 
-                'To see all commands use: /commands !');
+            this.bot.sendMessage(msg.chat.id, 'Hello, I will help you to improve your arithmetic skills.');
+            this.bot.sendMessage(msg.chat.id, 'To see all commands use: /commands !')
         })
     }
 }
