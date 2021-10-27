155e07052642877087198eb47e58250b4b4bb3e7
code0450 <atomsole567@gmail.com>
2021-10-27 12:09:04 +0500

    dsasadasda

--- src/actions/callback-query.ts
+++ src/actions/callback-query.ts
@@ -8,6 +8,36 @@ export class CallbackQuery {
     }
 
     init() {
+        this.bot.on('firstLevel', async msg => {
+            await this.bot.sendMessage(msg.chat.id, '!!question!!', {
+                reply_markup: JSON.stringify({
+                   inline_keyboard: [
+                       {text: 'Get answer', callback_data: 'the answer'}
+                   ]
+                })
+            });
+        });
+        this.bot.on('callback_query', async callback_query => {
+            await this.bot.answerCallbackQuery(callback_query.id, {url: `${process.env.URL}`});
+        })
+        this.bot.on('callback_query', async callback_query => {
+            await this.bot.answerCallbackQuery(callback_query.id, {url: `${process.env.URL}`});
+        })
+        this.bot.on('callback_query', async callback_query => {
+            await this.bot.answerCallbackQuery(callback_query.id, {url: `${process.env.URL}`});
+        })
+        this.bot.on('callback_query', async callback_query => {
+            await this.bot.answerCallbackQuery(callback_query.id, {url: `${process.env.URL}`});
+        })
+        this.bot.on('callback_query', async callback_query => {
+            await this.bot.answerCallbackQuery(callback_query.id, {url: `${process.env.URL}`});
+        })
+        this.bot.on('callback_query', async callback_query => {
+            await this.bot.answerCallbackQuery(callback_query.id, {url: `${process.env.URL}`});
+        })
+        this.bot.on('callback_query', async callback_query => {
+            await this.bot.answerCallbackQuery(callback_query.id, {url: `${process.env.URL}`});
+        })
         this.bot.on('callback_query', async callback_query => {
             await this.bot.answerCallbackQuery(callback_query.id, {url: `${process.env.URL}`});
         })
