fb9c6bc7c00ba9d8d2845d7737d3bdf6e090f520
code0450 <atomsole567@gmail.com>
2021-10-27 13:06:10 +0500

    sadasdas

--- src/actions/callback-query.ts
+++ src/actions/callback-query.ts
@@ -9,31 +9,7 @@ export class CallbackQuery {
 
     init() {
         this.bot.on('callback_query', async query => {
-            console.log(query.message.chat.id, query.data)
+            
         });
-        this.bot.on('callback_query', async callback_query => {
-            await this.bot.answerCallbackQuery(callback_query.id, {url: `${process.env.URL}`});
-        })
-        this.bot.on('callback_query', async callback_query => {
-            await this.bot.answerCallbackQuery(callback_query.id, {url: `${process.env.URL}`});
-        })
-        this.bot.on('callback_query', async callback_query => {
-            await this.bot.answerCallbackQuery(callback_query.id, {url: `${process.env.URL}`});
-        })
-        this.bot.on('callback_query', async callback_query => {
-            await this.bot.answerCallbackQuery(callback_query.id, {url: `${process.env.URL}`});
-        })
-        this.bot.on('callback_query', async callback_query => {
-            await this.bot.answerCallbackQuery(callback_query.id, {url: `${process.env.URL}`});
-        })
-        this.bot.on('callback_query', async callback_query => {
-            await this.bot.answerCallbackQuery(callback_query.id, {url: `${process.env.URL}`});
-        })
-        this.bot.on('callback_query', async callback_query => {
-            await this.bot.answerCallbackQuery(callback_query.id, {url: `${process.env.URL}`});
-        })
-        this.bot.on('callback_query', async callback_query => {
-            await this.bot.answerCallbackQuery(callback_query.id, {url: `${process.env.URL}`});
-        })
     }
 }
--- src/actions/game-act.ts
+++ src/actions/game-act.ts
@@ -9,9 +9,9 @@ export class GameAct {
         this.levelOptions = {
             reply_markup: JSON.stringify({
                 inline_keyboard: [
-                    [{text: '1', callback_data: 'firstLevel'}, {text: '2', callback_data: 'secondLevel'}, {text: '3', callback_data: 'thirdLevel'}],
-                    [{text: '4', callback_data: 'fourthLevel'}, {text: '5', callback_data: 'fifthLevel'}, {text: '6', callback_data: 'sixthLevel'}],
-                    [{text: '7', callback_data: 'seventhLevel'}, {text: '8', callback_data: 'eighthLevel'}, {text: '9', callback_data: 'ninethLevel'}]
+                    [{text: '1', callback_data: '1'}, {text: '2', callback_data: '2'}, {text: '3', callback_data: '3'}],
+                    [{text: '4', callback_data: '4'}, {text: '5', callback_data: '5'}, {text: '6', callback_data: '6'}],
+                    [{text: '7', callback_data: '7'}, {text: '8', callback_data: '8'}, {text: '9', callback_data: '9'}]
                 ]
             })
         }
