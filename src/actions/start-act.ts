cf07210cd3a5e3f13f2161d13fc9dae711898e74
code0450 <atomsole567@gmail.com>
2021-10-27 11:20:01 +0500

    asdasda

--- src/actions/start-act.ts
+++ src/actions/start-act.ts
@@ -10,8 +10,6 @@ export class StartAct {
     init() {
         this.bot.onText(/\/start/,async msg => {
             await this.bot.sendMessage(msg.chat.id, 'Hello, I will help you to improve your arithmetic skills.');
-            await this.bot.sendMessage(msg.chat.id, '/game - to launch the game\n' + 
-                '/help   - to get info about the game')
         })
     }
 }
