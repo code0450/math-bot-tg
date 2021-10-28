16619a1a8a7e3568bd43aebc72e14b030e554c87
code0450 <atomsole567@gmail.com>
2021-10-28 14:05:35 +0500

    asdasd

--- src/actions/help-act.ts
+++ src/actions/help-act.ts
@@ -10,7 +10,7 @@ export class HelpAct {
     init() {
         this.bot.onText(/\/help/, async msg => {
             await this.bot.sendMessage(msg.chat.id, 'Each level consists of audio questions, ' + 
-                'your objective is to solve at least 10 of them. ' +
+                'try to solve at least 10 of them. ' +
                 'Start from the level 1 and move on as you develop the skill.')
         })
     }
