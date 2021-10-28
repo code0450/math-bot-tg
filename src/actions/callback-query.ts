d7e1ce5c0bdf67b205c699482096c2fecb1348eb
code0450 <atomsole567@gmail.com>
2021-10-28 12:46:24 +0500

    asdasdasdasdasdasd

--- src/actions/callback-query.ts
+++ src/actions/callback-query.ts
@@ -11,7 +11,7 @@ export class CallbackQuery {
     init() {
         this.bot.on('callback_query', async query => {
             let audio = await Speech.getInstance().generate('this is an amazing test');
-            await this.bot.sendAudio(query.chat.id, audio)
+            await this.bot.sendAudio(query.message.chat.id, audio)
         });
     }
 }
